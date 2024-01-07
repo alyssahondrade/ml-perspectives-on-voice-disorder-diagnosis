aad7e9c92dd205875eb7ffe4f19448b4c853e6a2
Alyssa H <alyssa_hondrade@icloud.com>
2024-01-07 11:26:03 +0800

    working on preprocessing functions, calculated engineered features

--- voice_app/utils/preprocessing.py
+++ voice_app/utils/preprocessing.py
@@ -3,4 +3,18 @@ import streamlit as st
 from pprint import pprint
 
 def meta_preprocessing(metadata_dict):
+    
+    # Calculate reflux_indicated
+    rsi_score = metadata_dict['rsi_score']
+    if rsi_score >= 13:
+        metadata_dict['reflux_indicated'] = 1
+    else:
+        metadata_dict['reflux_indicated'] = 0
+
+    # Calculate vhi_zscore, based on research paper
+    normal_mean = 8.75
+    stddev = 14.97
+    equation = (metadata_dict['vhi_score'] - normal_mean) / stddev
+    metadata_dict['vhi_zscore'] = round(equation, 2)
+    
     pprint(metadata_dict)
