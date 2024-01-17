530d35c8e3c1bc06ee5d420a6c99bc938f611847
Alyssa H <alyssa_hondrade@icloud.com>
2024-01-17 11:17:17 +0800

    code cleanup, all functional

--- voice_app/utils/visualisation.py
+++ voice_app/utils/visualisation.py
@@ -7,29 +7,6 @@ import matplotlib.pyplot as plt
 import os
 from PIL import Image as pil_Image
 
-# def display_waveform(filepath_audio):
-#     """
-#     Purpose:
-#     - Display audio file's waveform.
-
-#     Input: filepath_audio
-#     Output: Displays plot
-#     """
-
-#     # Load the audio file using librosa
-#     y, sr = librosa.load(
-#         filepath_audio,
-#         sr = None # preserve sampling rate
-#     )
-
-#     # Plot the waveform
-#     librosa.display.waveshow(y, sr=sr)
-
-#     # Add the labels
-#     plt.title('Waveform')
-#     plt.xlabel('Time (s)')
-#     plt.ylabel('Amplitude')
-
     
 def load_audio(filepath_audio):
     """
